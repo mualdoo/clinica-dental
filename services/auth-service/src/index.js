21b3c752b0b1a254b743e516273e768b12c1a9f8
aldop-11 <aldoperezm30@outlook.com>
2026-04-21 00:07:10 -0600

    cors removed from services

--- services/auth-service/src/index.js
+++ services/auth-service/src/index.js
@@ -1,6 +1,5 @@
 import 'dotenv/config'
 import express from 'express'
-import cors from 'cors'
 
 import routes from './routes/user-routes.js'
 import { syncDatabase } from './models/index.js'
@@ -14,12 +13,11 @@ syncDatabase().then(() => {
 })
 startConsumers()
 
-app.use(cors())
 app.use(express.json())
 
 app.use('/', routes)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`auth-service corriendo en puerto ${PORT}`)
 })
